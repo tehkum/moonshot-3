aa739c83582629fadc538c8e22a96285fd0bc4ff
tehkum kanchwala <tehkumkanchwala@gmail.com>
2024-03-19 22:38:04 +0530

    Add .env file and update interests page

--- app/(pages)/interests/page.tsx
+++ app/(pages)/interests/page.tsx
@@ -1,19 +1,61 @@
 "use client";
 
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { faker } from "@faker-js/faker";
 
-export default function Signup() {
+export default function Interests() {
+  const [categories, setCategories] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 6;
+
+  useEffect(() => {
+    const newCategories: any = Array.from({ length: 100 }, () =>
+      faker.commerce.department()
+    );
+    setCategories(newCategories);
+  }, []);
+
+  useEffect(() => {
+
+    const start = (currentPage - 1) * itemsPerPage;
+    const end = start + itemsPerPage;
+    const visibleCategories = categories.slice(start, end);
+    setCategories(visibleCategories);
+  }, [currentPage]);
+
+  const totalPages = Math.ceil(categories.length / itemsPerPage);
+
   return (
     <div className="signup-container">
       <h1>Please mark your interests</h1>
 
       <div className="login-welcome">
-        <h3>Welcome back to ECOMMERCE</h3>
-        <p>The next gen business marketplace</p>
+        <p>We will keep you notified</p>
       </div>
 
-      
+      {categories.map((category, index) => (
+        <div key={index} className="align-center">
+          <input
+            type="checkbox"
+            id={`category-${index}`}
+            name={`category-${index}`}
+          />
+          <label htmlFor={`category-${index}`}>{category}</label>
+        </div>
+      ))}
+
+      <div className="pagination">
+        {Array.from({ length: totalPages }, (_, i) => (
+          <button
+            key={i}
+            onClick={() => setCurrentPage(i + 1)}
+            className={currentPage === i + 1 ? "active" : ""}
+          >
+            {i + 1}
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
